7424af868a894876f2d61b9bf360422c41302556
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-12-19 19:47:05 -0700

    upd

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,10 +1,36 @@
-// async function fetchCatByBreed(breedId) {
-//   return (data = await axios
-//     .get('/images/search', {
-//       params: {
-//         limit: 10,
-//         breed_ids: breedId,
-//       },
-//     })
-//     .then(resp => console.log(resp)));
-// };
+import axios from 'axios';
+import { refs } from './index';
+import { createMarkup } from './index';
+import { Notify } from 'notiflix';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_zFksffvjV5C1VI3jqdVkoP6lGz4eQnTFRekdHtefmwojLFX28yJABEnBPGHJIB1T';
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
+
+async function fetchBreeds() {
+  return (data = await axios
+    .get('/breeds')
+    .then(resp => resp.data.map(data => data))
+    .catch(err => {
+      console.error(err);
+      Notify.failure('Oops! Something went wrong! Try reloading the page!');
+      refs.catInfo.innerHTML = '';
+    }));
+}
+
+async function fetchCatByBreed(breedId) {
+  return (data = await axios
+    .get('/images/search', {
+      params: {
+        breed_ids: breedId,
+      },
+    })
+    .then(resp => (refs.catInfo.innerHTML = createMarkup(resp)))
+    .catch(err => {
+      console.error(err);
+      Notify.failure('Oops! Something went wrong! Try reloading the page!');
+      refs.catInfo.innerHTML = '';
+    }));
+}
+
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
-import axios from 'axios';
 import '../node_modules/modern-normalize/modern-normalize.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SlimSelect from 'slim-select';
-// import fetchCatByBreed from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api'
 
-const refs = {
+
+export const refs = {
   breedSelect: document.querySelector('.breed-select'),
   catInfo: document.querySelector('.cat-info'),
 };
 
-axios.defaults.headers.common['x-api-key'] = 'live_zFksffvjV5C1VI3jqdVkoP6lGz4eQnTFRekdHtefmwojLFX28yJABEnBPGHJIB1T';
-axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
 
-async function fetchBreeds() {
-  return (data = await axios
-    .get('/breeds')
-    .then(resp => resp.data.map(data => data))
-    .catch(err => {
-      console.error(err);
-    }));
-}
 
 // Create options markup -->
 fetchBreeds().then(data =>
@@ -31,20 +19,6 @@ fetchBreeds().then(data =>
   )
 );
 
-function fetchCatByBreed(breedId) {
-  return (data = axios
-    .get('/images/search', {
-      params: {
-        breed_ids: breedId,
-      },
-    })
-    .then(resp => (refs.catInfo.innerHTML = createMarkup(resp)))
-    .catch(err => {
-      Notify.failure('Oops! Something went wrong! Try reloading the page!');
-      refs.catInfo.innerHTML = '';
-    }));
-}
-
 refs.breedSelect.addEventListener('change', onSelect);
 
 function onSelect() {
@@ -52,16 +26,18 @@ function onSelect() {
   fetchCatByBreed(refs.breedSelect.value);
 }
 
-function createMarkup(arr) {
+export function createMarkup(arr) {
   return arr.data.map(
     ({ breeds: [{ name, temperament, description }], url }) => {
       return `
-      <img src="${url}" alt="${name}">
-<div class='text-container'>
-        <h1>${name}</h1>
-        <p>${description}</p>
-        <span><b>Temperament: </b>${temperament}</span>
-</div>
+      <div class='container'>
+        <img src="${url}" alt="${name}">
+  <div class='text-container'>
+          <h1>${name}</h1>
+          <p>${description}</p>
+          <span><b>Temperament: </b>${temperament}</span>
+  </div>
+      </div>
     `;
     }
   );
